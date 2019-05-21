4298793b03af2657987b458c60effacba8ac791d
irmillar <50762772+irmillar@users.noreply.github.com>
2019-05-21 20:20:33 +1000

    Update settings.py adding 'ALLOWED_HOSTS'
    Added "imillar.pythonanywhere.com" to "ALLOWED_HOSTS" to allow this project to run as a web app on pythonanywhere

--- learning_users/learning_users/settings.py
+++ learning_users/learning_users/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '78sy4c_ub+o&$fp__7r$iew*_l@&d0xlh3%n0gbl58o639q&_8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["imillar.pythonanywhere.com"]
 
 
 # Application definition
